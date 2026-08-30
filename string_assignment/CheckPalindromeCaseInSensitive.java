59e7a880fc809ec0cc421a9e18a9cb04900f1570
Yogesh Sakhare <154612218+YogeshSakhare@users.noreply.github.com>
2026-08-30 22:32:25 +0530

    Check Palindrome Case_Insensitive

--- string_assignment/CheckPalindromeCaseInSensitive.java
+++ string_assignment/CheckPalindromeCaseInSensitive.java
@@ -1,4 +1,17 @@
 class CheckPalindromeCaseInSensitive{
 	public static void main(String args[]){
+		String s1="Nayan";
+		String s2=s1.toLowerCase();
+		int start=0;
+		int end=s2.length()-1;
+		while(start<end){
+			if(s2.charAt(start)!=s2.charAt(end)){
+				System.out.println(s1+" is not a Palindrome String");
+				return;
+			}
+			start++;
+			end--;
+		}
+		System.out.println(s1+" is a Palindrome String");
 	}
 }
