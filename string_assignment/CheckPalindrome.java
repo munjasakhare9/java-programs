070dbd369f64f948bd13ddf6855e532e259fe692
Yogesh Sakhare <154612218+YogeshSakhare@users.noreply.github.com>
2026-08-30 22:14:39 +0530

    check palindrome or not

---
+++ string_assignment/CheckPalindrome.java
@@ -0,0 +1,16 @@
+class CheckPalindrome{
+	public static void main(String args[]){
+		String s1="nayan";
+		int start=0;
+		int end=s1.length()-1;
+		while(start<end){
+			if(s1.charAt(start)!=s1.charAt(end)){
+				System.out.println(s1+"is Not a Palindrome");
+				return;
+			}
+			start++;
+			end--;
+		}
+		System.out.println(s1+" is a Palindrome String");
+	}
+}
