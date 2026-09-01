ba92bf566c6348d9f06e569bc62adb15384f4fb2
Yogesh Sakhare <154612218+YogeshSakhare@users.noreply.github.com>
2026-09-01 17:16:40 +0530

    check whether array is sorted or not

--- array_assignment/Program5.java
+++ array_assignment/Program5.java
@@ -2,7 +2,7 @@ class Program5{
 	public static void main(String args[]){
 		int arr[]={1,2,3,4,5,9,6,7,8,9};
 		for(int i=0;i<arr.length-1;i++){
-			if(!(arr[i]<arr[i+1])){
+			if(!(arr[i]<=arr[i+1])){
 				System.out.println("Array is not sorted..");
 				return;
 			}
