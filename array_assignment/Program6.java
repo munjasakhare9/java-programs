479bb85959044d967d1b3b7d9972e15d78c67313
Yogesh Sakhare <154612218+YogeshSakhare@users.noreply.github.com>
2026-09-01 17:29:23 +0530

    find second largest distinct element

---
+++ array_assignment/Program6.java
@@ -0,0 +1,17 @@
+class Program6{
+	public static void main(String args[]){
+		int arr[]={1,2,5,6,7,9,8,4,2,5,6,9};
+		int max=Integer.MIN_VALUE;
+		int secondMax=Integer.MIN_VALUE;
+		for(int i=0;i<arr.length;i++){
+			if(max<arr[i]){
+				secondMax=max;
+				max=arr[i];
+			}
+			else if(secondMax<arr[i] && arr[i]<max){
+				secondMax=arr[i];
+			}
+		}
+		System.out.println("Second Maximum Element is :- "+secondMax);
+	}
+}
