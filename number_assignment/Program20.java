a993105721c4a1739a20cfb0782c62a0bfa390dd
Yogesh Sakhare <154612218+YogeshSakhare@users.noreply.github.com>
2026-09-02 21:23:18 +0530

    Check if a number is a Buzz Number (ends with 7 or is divisible by 7).

---
+++ number_assignment/Program20.java
@@ -0,0 +1,11 @@
+class Program20{
+	public static void main(String args[]){
+		int num=177;
+		if(num%10==7 || num%7==0){
+			System.out.println(num+" is Buzz Number");
+		}
+		else{
+			System.out.println(num+" is not a Buzz Number");
+		}
+	}
+}
