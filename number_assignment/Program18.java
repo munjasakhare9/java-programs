79aab5753a916d7c13c4577f5ef336ce832d6790
Yogesh Sakhare <154612218+YogeshSakhare@users.noreply.github.com>
2026-09-02 18:27:29 +0530

    Check if a number is a Spy Number

---
+++ number_assignment/Program18.java
@@ -0,0 +1,20 @@
+class Program18{
+	public static void main(String args[]){
+		int num=11124;
+		int temp=num;
+		int sum=0;
+		int product=1;
+		while(temp!=0){
+			int digit=temp%10;
+			sum+=digit;
+			product*=digit;
+			temp/=10;
+		}
+		if(sum==product){
+			System.out.println(num+" Its Spy number :- "+"sum="+sum+" product ="+product);
+		}
+		else{
+			System.out.println(num+" Its not Spy number :- "+"sum="+sum+" product ="+product);
+		}
+	}
+}
