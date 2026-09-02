cb251dfbef08595c94d04388f1801a08c9f69213
Yogesh Sakhare <154612218+YogeshSakhare@users.noreply.github.com>
2026-09-02 22:23:48 +0530

    remove white spaces and tabs

---
+++ string_assignment/Program11.java
@@ -0,0 +1,9 @@
+class Program11{
+    public static void main(String args[]){
+        String s1=" i am java developer ";
+        
+        s1=s1.replaceAll("\\s+", "");
+        
+        System.out.println(s1);
+    }
+}
