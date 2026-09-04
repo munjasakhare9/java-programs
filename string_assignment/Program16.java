80511ab2d0411bed74f17cde30d21f1656c94f63
Yogesh Sakhare <154612218+YogeshSakhare@users.noreply.github.com>
2026-09-04 13:18:42 +0530

    Find Common Prefix of Two Strings

--- string_assignment/Program16.java
+++ string_assignment/Program16.java
@@ -1,24 +1,24 @@
 class Program16{
-	public static void main(String args[]){
-		String s1="hello";
-		String s2="hello";
-		for(int i=0;i<s1.length() && i<s2.length();i++){
-			char ch1=s1.charAt(i);
-			char ch2=s2.charAt(i);
-			if(ch1==ch2){
-				if(i==s1.length()-1 || i==s2.length()){
-					System.out.println(s1.substring(0,i+1));
-					return;
-				}
-			}
-			else if(ch1!=ch2 && i!=0){
-				System.out.println(s1.substring(0,i+1));
-				return;
-			}
-			else{
-				break;
-			}
-		}
-		System.out.println("-1");
-	}
+    public static void main(String args[]){
+        String s1="hello i am java developer";
+        String s2="hello i am python developer";
+
+        int i=0;
+
+        while(i<s1.length() && i<s2.length()){
+
+            if(s1.charAt(i)!=s2.charAt(i)){
+                break;
+            }
+
+            i++;
+        }
+
+        if(i==0){
+            System.out.println("-1");
+        }
+        else{
+            System.out.println(s1.substring(0,i));
+        }
+    }
 }
