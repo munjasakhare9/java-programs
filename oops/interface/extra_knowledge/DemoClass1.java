da02823519818f6a48451df8c2ed6555ec3b516a
Yogesh Sakhare <154612218+YogeshSakhare@users.noreply.github.com>
2026-09-06 10:20:13 +0530

    new added

---
+++ oops/interface/extra_knowledge/DemoClass1.java
@@ -0,0 +1,16 @@
+class DemoClass1 implements Demo1{
+	public void m1(){
+		System.out.println("m1 method of DemoClass1");
+	}
+	
+	public void m2(){
+		System.out.println("m2 method of DemoClass1");
+	}
+	
+	public void m4(){
+		System.out.println("m4 method of DemoClass1");
+	}
+	public void toString(){
+			System.out.println("i am munja");
+	}
+}
---
+++ oops/interface/extra_knowledge/MainClass.java
@@ -0,0 +1,9 @@
+class MainClass{
+	public static void main(String args[]){
+		DemoClass1 d1=new DemoClass1();
+		d1.m1();
+		d1.m2();
+		d1.m3();
+		d1.toString();
+	}
+}
