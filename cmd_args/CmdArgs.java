a0cb2a76e53ceb9868bf4a0b4dec338ce8eba23d
Munja Sakhare <154612218+munjasakhare9@users.noreply.github.com>
2026-09-14 13:34:42 +0530

    input from cmd args

---
+++ cmd_args/CmdArgs.java
@@ -0,0 +1,14 @@
+class CmdArgs{
+	public static void main(String args[]){
+		System.out.println ("Total Cmd Args :: " + args.length );
+
+		String s1 = args[0];
+
+		String s2 = args[1];
+
+		int x = Integer.parseInt (s1);
+		int y = Integer.parseInt(s2);
+
+		System.out.println (x + y);
+	}	
+}
